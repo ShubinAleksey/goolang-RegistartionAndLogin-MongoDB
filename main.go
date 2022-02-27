28d1b1e28b4431fc1e51fb0dc5ec42b6a9f33f2f
ShubinAleksey <mrabdulrhman9@gmail.com>
2022-02-27 15:37:22 +0300

    main

--- main.go
+++ main.go
@@ -10,16 +10,22 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"gopkg.in/validator.v2"
+
+	"github.com/go-playground/validator"
 )
 
 type User struct {
 	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Login    string             `json:"login,omitempty" bson:"login,omitempty" validate:"min=3,max=50"`
-	Email    string             `json:"email,omitempty" bson:"email,omitempty" validate:"regexp=^([A-Za-z]|[0-9])+@"`
+	Email    string             `json:"email,omitempty" bson:"email,omitempty" validate:"required,email"`
 	Password string             `json:"password,omitempty" bson:"password,omitempty" validate:"min=6,max=50"`
 }
 
+type Result struct {
+	ResultMessage string `json:"resultmessage,omitempty" bson:"resultmessage,omitempty"`
+}
+
+var validate *validator.Validate
 var user User
 var ctx, cancel = context.WithTimeout(context.Background(), 25*time.Second)
 var client, erring = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
@@ -29,10 +35,10 @@ func JSONregistration(c *gin.Context) {
 	c.Writer.Header().Add("content-type", "application/json")
 	json.NewDecoder(c.Request.Body).Decode(&user)
 	ctx, _ := context.WithTimeout(context.Background(), 25*time.Second)
-	if err := validator.Validate(user); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"Ошибка": err.Error(),
-		})
+	validate = validator.New()
+	if err := validate.Struct(user); err != nil {
+		c.Writer.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(c.Writer).Encode(Result{ResultMessage: err.Error()})
 	} else {
 		collection.InsertOne(ctx, user)
 		json.NewEncoder(c.Writer).Encode(http.StatusOK)
@@ -46,7 +52,7 @@ func JSONLogin(c *gin.Context) {
 	err := collection.FindOne(ctx, user).Decode(&user)
 	if err != nil {
 		c.Writer.WriteHeader(http.StatusInternalServerError)
-		c.Writer.Write([]byte(`{ "Ошибка": "` + err.Error() + `"}`))
+		json.NewEncoder(c.Writer).Encode(Result{ResultMessage: err.Error()})
 		return
 	} else {
 		json.NewEncoder(c.Writer).Encode(http.StatusOK)
